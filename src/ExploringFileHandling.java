7624ee7fef2a05046a81be4037d2a5e47c4d14bf
Bhaskar Pilania <bpilania@gmail.com>
2013-10-29 04:05:37 -0400

    Nexsan code enhanced

--- src/ExploringFileHandling.java
+++ src/ExploringFileHandling.java
@@ -10,18 +10,21 @@ import java.io.OutputStream;
 
 public class ExploringFileHandling {
 
-	/**
-	 * @param args
+
+	/*
+	 * 
+	 * This function reads 4K buffer iteratively till end of file. Each time it reads the buffer, it calls findCount function
+	 * to find counts of relevant strings
 	 */
-
-	static Result findTotalNexsanCounts(String fileName) {
+	static ResultCounts findTotalNexsanCounts(String fileName) {
+		int fileBufferSize = 4 * 1024;
+		
 		InputStream input = null;
-		int countNexsan = 0;
-		int countNexsanTech = 0;
-
 		long totalBytesRead = 0;
 		long remainingBytes = 0;
-		Result result = new Result();
+
+		ResultCounts result = new ResultCounts();
+		
 		try {
 
 			File file = new File(fileName);
@@ -30,7 +33,7 @@ public class ExploringFileHandling {
 			System.out.println(totalBytes);
 
 			remainingBytes = totalBytes;
-			byte[] buffer = new byte[300];
+			byte[] buffer = new byte[fileBufferSize];
 			char[] charBuffer = new char[buffer.length];
 
 			while (remainingBytes > 0) {
@@ -38,7 +41,7 @@ public class ExploringFileHandling {
 						: buffer.length;
 
 				input = new BufferedInputStream(new FileInputStream(file));
-				buffer = new byte[300];
+				buffer = new byte[fileBufferSize];
 				charBuffer = new char[buffer.length];
 				input.skip(totalBytesRead);
 				int bytesRead = input.read(buffer, (int) 0, (int) lengthToRead);
@@ -50,64 +53,128 @@ public class ExploringFileHandling {
 						charBuffer[i] = (char) buffer[i];
 
 					findCount(charBuffer, result, remainingBytes);
-
+					
+					/* In case there are more characters to read i.e. if buffer is not full, we will rollback 19 bytes 
+					so that we can count any missed/incomplete 'Nexsan' and 'Nexsan Technologies' 
+					
+					In this process I may count Nexsan twice since it can fit in 18 bytes and could have been counted earlier.
+					To ensure this does not happen, findCount method does not count "Nexsan" strings that are in last 18 bytes of buffer. 
+					They are counted only when they appear next time at starting of buffer since I am rolling back buffer by 18 bytes.
+					
+					But this done only if i have remaining bytes to read. When there is nothing to read, I do not roll back and hence count
+					whatever is there which is the correct behaviour. 
+					*/
+					
 					if (remainingBytes > 0) {
 						totalBytesRead = totalBytesRead - 18;
 						remainingBytes = totalBytes - totalBytesRead;
 					}
-					System.out.println("totalBytesRead: "+totalBytesRead);
-					System.out.println("remainingBytes: "+remainingBytes);
+
 				}
 			}
 		} catch (FileNotFoundException e) {
 			System.out.println("File Not Found");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Error Accessing File!");
 		} finally {
 			try {
 				input.close();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				System.out.println("Error Closing File!");
 			}
 		}
 
 		return result;
 	}
 
-	static void findCount(char[] arr, Result result, long remainingBytes) {
-
-		int countNexsan = 0;
-		int countNexsanTech = 0;
-		boolean foundNexsan = false;
-
-		boolean foundNexsanTech = false;
-
-		int maxLength = arr.length;
+	/*
+	 * findCount function returns the case sensitive and case insensitive counts of Nexsan and Nexsan Technologies 
+	 * 
+	 * RegEx are not allowed. I have not used them. But I am assuming library functions are allowed to find index 
+	 * of a string in the buffer. I still have to parse the occurrence to check if it has valid delimiters.
+	 * 
+	 * Logic: 
+	 * 1. Reads a buffer of 4KB and finds all case sensitive and case insensitive counts of Nexsan and Nexsan Technologies 
+	 * 2. Some incomplete part of Nexsan and Nexsan Technologies may occur at end of buffer. To take care of that,
+	 * 		2.1  I use an overlapping window of buffers. Each new buffer read starts from (currentPosition - 18) bytes
+	 *		2.2  This helps in counting any string which was cut in last buffer
+	 *		2.3  I am reading previous 18 bytes but not 19 because in case of 19 bytes "Nexsan Technologies" would have 
+	 *			 completely fit
+	 *		2.4  
+	 * 
+	 */
+	static void findCount(char[] arr, ResultCounts result, long remainingBytes) {
 		String bufferOriginal = new String(arr);
 		bufferOriginal = bufferOriginal.trim();
-		String bufferSmallCased = bufferOriginal.toLowerCase();
+		
+		//function call which gets case sensitive and case insensitive counts in result object
+		findCountHelper(bufferOriginal, true, result, remainingBytes);
+		findCountHelper(bufferOriginal, false, result, remainingBytes);
+
+		//In following loop I am counting number of Complete occurrences of "valid delimited strings"
+
+
+	}
+	
+	static void findCountHelper(String bufferOriginal, boolean isCaseSensitive, ResultCounts result, long remainingBytes){
+		int countNexsan = 0;
+		int countNexsanTech = 0;
 
 		String nexsan = "Nexsan";
 		String nexsanTech = "Nexsan Technologies";
-
-		int indexNexsan = 0;
-		while (indexNexsan > -1) {
-			indexNexsan = bufferOriginal.indexOf(nexsan, indexNexsan);
-			int indexNexsanTech = bufferOriginal.indexOf(nexsanTech,
-					indexNexsan);
-			if (indexNexsan >= 0) {
-				if (indexNexsan == indexNexsanTech)
-					countNexsanTech++;
-				else
-					countNexsan++;
-
-				indexNexsan++;
-			}
+		if(!isCaseSensitive){
+			bufferOriginal = bufferOriginal.toLowerCase();
+			nexsan = nexsan.toLowerCase();
+			nexsanTech = nexsanTech.toLowerCase();
 		}
 
-		indexNexsan = bufferOriginal.length() - nexsanTech.length() +1;
+		int indexNexsan = 0;
+		int indexNexsanTech = 0;
+		boolean isNexsanCorrect = false;
+		boolean isNexsanTechCorrect = false;
+		System.out.println("CP1");
+		while (indexNexsan > -1 || indexNexsanTech > -1) {
+			indexNexsan = bufferOriginal.indexOf(nexsan, indexNexsan);
+			if(indexNexsan > -1){
+				isNexsanCorrect = nexsanDelimitChecker(bufferOriginal, indexNexsan);
+				if(!isNexsanCorrect)
+					indexNexsan = -1;
+			}
+			else
+				isNexsanCorrect = false;
+			
+			indexNexsanTech = bufferOriginal.indexOf(nexsanTech, indexNexsanTech);
+			if(indexNexsanTech > -1){
+				isNexsanTechCorrect = nexsanTechDelimitChecker(bufferOriginal, indexNexsanTech);
+				if(!isNexsanTechCorrect)
+					indexNexsanTech = -1;
+			}
+			else
+				isNexsanTechCorrect = false;
+			
+			
+			if (isNexsanCorrect || isNexsanTechCorrect) {
+				if (indexNexsan == indexNexsanTech){
+					countNexsanTech++;
+					indexNexsanTech += 19;
+					indexNexsan = indexNexsanTech;
+				}
+				else if(indexNexsan > -1){
+					countNexsan++;
+					indexNexsan += 6;
+					indexNexsanTech = indexNexsan;
+				}
+				else if(indexNexsan < 0){
+					countNexsanTech++;
+					indexNexsanTech += 19;
+					indexNexsan = indexNexsanTech;				
+				}
+
+			}
+		}
+		System.out.println("CP2");
+
+		indexNexsan = bufferOriginal.length() - nexsanTech.length() + 1;
 		while (remainingBytes > 0 && indexNexsan > -1) {
 			indexNexsan = bufferOriginal.indexOf(nexsan, indexNexsan);
 			if (indexNexsan > 0) {
@@ -115,58 +182,128 @@ public class ExploringFileHandling {
 				indexNexsan++;
 			}
 		}
-		result.nexsanCount += countNexsan;
-		result.nexsanTechCount += countNexsanTech;
-
-	}
-
-	static void write(byte[] aInput, String aOutputFileName) {
-		System.out.println("Writing binary file...");
-		try {
-			OutputStream output = null;
-			try {
-				output = new BufferedOutputStream(new FileOutputStream(
-						aOutputFileName));
-				output.write(aInput);
-			} finally {
-				output.close();
-			}
-		} catch (FileNotFoundException ex) {
-			System.out.println("File not found.");
-		} catch (IOException ex) {
-			System.out.println(ex);
+		if(isCaseSensitive){
+			result.setNexsanCountCS(result.getNexsanCountCS() + countNexsan);
+			result.setNexsanTechCountCS(result.getNexsanTechCountCS() + countNexsanTech);
+		}else{
+			result.setNexsanCountNCS(result.getNexsanCountNCS() + countNexsan);
+			result.setNexsanTechCountNCS(result.getNexsanTechCountNCS() + countNexsanTech);
 		}
+			
+	}
+	
+	static boolean nexsanDelimitChecker(String str, int index){
+		
+		if(index > (str.length() - 1) - 19)
+			return false;						
+		
+		int endChar = str.charAt(index+6);
+		if((endChar >= 48 && endChar <= 57) || (endChar >= 65 && endChar <= 90) || (endChar >= 97 && endChar <= 122))
+			return false;
+		
+		if(index > 0){
+			int startChar = str.charAt(index-1);
+			if((startChar >= 48 && startChar <= 57) || (startChar >= 65 && startChar <= 90) || (startChar >= 97 && startChar <= 122))
+				return false;
+		}
+			
+		return true;
+	}
+	
+	static boolean nexsanTechDelimitChecker(String str, int index){
+		
+		if(index > (str.length() - 1) - 19)
+			return false;						
+		
+		int endChar = str.charAt(index+19);
+		if((endChar >= 48 && endChar <= 57) || (endChar >= 65 && endChar <= 90) || (endChar >= 97 && endChar <= 122))
+			return false;
+		
+		if(index > 0){
+			int startChar = str.charAt(index-1);
+			if((startChar >= 48 && startChar <= 57) || (startChar >= 65 && startChar <= 90) || (startChar >= 97 && startChar <= 122))
+				return false;
+		}
+			
+		return true;
+
 	}
 
 	public static void main(String args[]) throws IOException {
-		char c;
-		// Create a BufferedReader using System.in
-		String test = "HelloThNexsanisIsBhaskarNexsanNexsan TechnologiesNexsNexsansIsBhaskarNexsanNexsan TechnologiesNisIsBhaskarNexsanNexsan TechnolnNexsan TechnologiesNexsNexsanisIsBhaskarNexsanNexsan TechnologiesNexsNexsanisIsBhaskarNexsanNexsan TechnologiesNexsan NexsanHelloThNexsanisIs";
-		char[] testArr = test.toCharArray();
-
-		System.out.println("Enter characters, 'q' to quit.");
-		// read characters
-		int i = 0;
-
-		byte[] arr = new byte[testArr.length];
-		for (i = 0; i < testArr.length && testArr[i] != 0; i++) {
-			arr[i] = (byte) testArr[i];
-
+		String fileName;
+		try{
+		fileName = args[0];
+		}catch(ArrayIndexOutOfBoundsException e){
+			System.out.println("Filename not specified in argument!");
+			System.exit(0);
 		}
+		
+		ResultCounts result = findTotalNexsanCounts("TestFile");
+		
+		/*Priority of case insensitive strings is kept lower. i.e. If a string has been counted as 
+		already been counted in case sensitive, it cannot be shown again in case insensitive.
+		To ensure this, I have subtracted the case sensitive counts from insensitive counts*/ 
+		
+		System.out.println("Total Case-Sensitive Nexsan Count is: "+result.getNexsanCountCS());
 
-		write(arr, "TestFile");
+		System.out.println("Total Case-Sensitive Nexsan Technologies Count is: "+result.getNexsanTechCountCS());
+		
 
-		arr = new byte[20];
+		
+		System.out.println("Total Case-InSensitive Nexsan Count is: "+(result.getNexsanCountNCS() - result.getNexsanCountCS()));
 
-		Result result = findTotalNexsanCounts("TestFile");
-
-		System.out.println(result.nexsanCount);
-		System.out.println(result.nexsanTechCount);
+		System.out.println("Total Case-InSensitive Nexsan Technologies Count is: "+(result.getNexsanTechCountNCS() - result.getNexsanTechCountCS()));
 
 	}
 }
 
-class Result {
-	int nexsanCount = 0;
-	int nexsanTechCount = 0;
+
+/* 
+ *  Wrapper Result Class which stores all relevant counts of strings.
+ */
+class ResultCounts {
+	private int nexsanCountCS;
+	private int nexsanTechCountCS;
+	private int nexsanCountNCS;
+	private int nexsanTechCountNCS;
+	
+	public ResultCounts(){
+		nexsanCountCS = 0;
+		nexsanTechCountCS = 0;
+		nexsanCountNCS = 0;
+		nexsanTechCountNCS = 0;	
+	}
+
+	public int getNexsanCountCS() {
+		return nexsanCountCS;
+	}
+
+	public void setNexsanCountCS(int nexsanCountCS) {
+		this.nexsanCountCS = nexsanCountCS;
+	}
+
+	public int getNexsanTechCountCS() {
+		return nexsanTechCountCS;
+	}
+
+	public void setNexsanTechCountCS(int nexsanTechCountCS) {
+		this.nexsanTechCountCS = nexsanTechCountCS;
+	}
+
+	public int getNexsanCountNCS() {
+		return nexsanCountNCS;
+	}
+
+	public void setNexsanCountNCS(int nexsanCountNCS) {
+		this.nexsanCountNCS = nexsanCountNCS;
+	}
+
+	public int getNexsanTechCountNCS() {
+		return nexsanTechCountNCS;
+	}
+
+	public void setNexsanTechCountNCS(int nexsanTechCountNCS) {
+		this.nexsanTechCountNCS = nexsanTechCountNCS;
+	}
+
 }
